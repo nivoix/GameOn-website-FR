30a9a3a7aee4aa48291c07d1456dc55e2b65b03b
nivoix <jnivoix@yahoo.fr>
2023-07-18 18:27:15 +0200

    check quantity ok

--- starterOnly/form.js
+++ starterOnly/form.js
@@ -3,7 +3,6 @@ const form = document.querySelector("form")
 // Ecoute le submit du formulaire
 form.addEventListener("submit", (e) => {
   e.preventDefault()
-  console.log(form);
   
   //récuperer les éléments du formulaire
   const first = document.getElementById("first")
@@ -14,16 +13,20 @@ form.addEventListener("submit", (e) => {
   const location = document.querySelectorAll('.formData input.checkbox-input[name=location]')
   const checkbox1 = document.getElementById("checkbox1")
   const checkbox2 = document.getElementById("checkbox2")
-  
+  console.log(location);
+  console.log(location[1].checked);
+  console.log(quantity.value);
   /////////// controle des inputs /////////////////////////////////
   
-  regexprenom= /^(([A-Za-zÀ-ÿœ]{2,40})([^ ])?(['.\ -]{0,5}))*$/;
+  regexprenom= /^([^ ])(([A-Za-zÀ-ÿœ]{2,40})?(['.\ -]{0,5}))*$/;
   msgprenom = "Chiffes et symboles interdits.Maxi 40, mini 2 caractères";
-  regexnom= /^(([A-Za-zÀ-ÿœ]{2,30})([^ ])?(['.\ -]{0,5}))*$/;
+  regexnom= /^([^ ])(([A-Za-zÀ-ÿœ]{2,30})?(['.\ -]{0,5}))*$/;
   msgnom = `Chiffes et symboles interdits.Maxi 30, mini 2 caractères`;
   regexemail= /^([^ ])[a-zA-Z0-9_.+-]+@[a-zA-Z]+\.[a-z]{2,4}$/;
   msgemail = `Veuillez indiquer une adresse email valide`;
-  msgbirthdate =`veuillez entrer une date valide`
+  msgbirthdate =`veuillez entrer une date valide`;
+  regexquantity =/^[0-9]{0,1}[0-9]$/;
+  msgquantity = `Veuillez indiquer une quantité entre 0 et 99`;
   
   function checkinput (regex, msg, inputValue,) {
     if(regex.test(inputValue)) {
@@ -44,11 +47,12 @@ form.addEventListener("submit", (e) => {
       console.log(msg);
       return false
     }
-
   }
+
   checkinput(regexprenom, msgprenom, first.value)
   checkinput(regexnom, msgnom, last.value)
   checkinput(regexemail, msgemail, email.value)
+  checkinput(regexquantity, msgquantity, quantity.value)
   checkbirthdate(msgbirthdate, birthdate.value)
   //////////////////////////////////////////////////////////////////////////////
 })
