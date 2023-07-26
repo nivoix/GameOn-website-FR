2c8ad44091c53bd80a852aaa2db3d65a42d118d0
nivoix <jnivoix@yahoo.fr>
2023-07-26 17:03:26 +0200

    correction responsive topnav

--- starterOnly/form.js
+++ starterOnly/form.js
@@ -5,18 +5,20 @@ form.addEventListener("submit", (e) => {
 })
 
 // validation du formulaire 
-function Validate() {
+function Validate(form) {
   //récuperer les éléments du formulaire
   const first = document.getElementById("first")
   const last = document.getElementById("last")
   const email = document.getElementById("email")
   const birthdate = document.getElementById("birthdate")
   const quantity = document.getElementById("quantity")
-  const location = document.querySelectorAll('.formData input.checkbox-input[name=location]')
   const checkbox1 = document.getElementById("checkbox1")
-  const checkbox2 = document.getElementById("checkbox2")
-  /////////// controle des inputs /////////////////////////////////
+  let inputcheck = false
+  let inputbirtdate = false
+  let inputlocation = false
+  let inputcheckbox1 = false
 
+  //expressions régullières et messages d'erreur
   regexprenom= /(^[A-Z]+[ \-'])?([[a-zA-ZÀ-ÿœé])+([ \-'])?]*([a-zA-ZÀ-ÿœ])+$/;
   msgprenom = "Chiffres et symboles interdits. Minimum 2 caractères.";
   regexnom= /(^[A-Z]+[ \-'])?([[a-zA-ZÀ-ÿœé])+([ \-'])?]*([a-zA-ZÀ-ÿœ])+$/;
@@ -29,16 +31,19 @@ function Validate() {
   msglocation = `Veuillez sélectionner la localisation d'un tournois.`;
   msgcheckbox1 = `Veuillez accepter les conditions d'utilisation.`
   
-
+// controle des inputs first, last et email
   function checkinput (regex, msg, inputValue, inputError) {
     if(regex.test(inputValue)){
       toggleAttribute(inputError)
+      inputcheck = true
       return true
     }else {
       setAttribute(msg, inputError)
       return false
     }
   }
+
+  //controle de la date d'anniversaire
   function checkbirthdate (msg, inputValue, inputError) {
     let todayDate = new Date()
     let year = todayDate.getFullYear()
@@ -46,6 +51,7 @@ function Validate() {
     let inputYearValue = inputYear[0]
     if((year-inputYearValue)>=18 && (year-inputYearValue)<100) {
       toggleAttribute(inputError)
+      inputbirtdate = true
       return true
     }else {
       setAttribute(msg, inputError)
@@ -53,26 +59,31 @@ function Validate() {
     }
   }
 
-  function checklocation(msg, inputValue, inputError) {
+  // controle de la localisation cochée
+  function checklocation(msg, inputError) {
   const checkboxes = document.querySelectorAll(".checkbox-input[type=radio]");
     if(Array.from(checkboxes).some((checkbox) => checkbox.checked)) {
-        toggleAttribute(inputError)
-        return true
-      } else{
-        setAttribute(msg, inputError)
-        return false
-      }
+      toggleAttribute(inputError)
+      inputlocation = true
+      return true
+    } else{
+      setAttribute(msg, inputError)
+      return false
+    }
   }
 
+  //controle des conditions d'utilisation cochées
   function checkcheckbox1(msg, inputValue, inputError){
     if(inputValue) {
       toggleAttribute(inputError)
+      inputcheckbox1 = true
       return true
     } else{
       setAttribute(msg, inputError)
     }
   }
   
+  // 2 fonctions pour afficher ou non le message d'erreur avec son style
   function toggleAttribute(inputError) {
     let formDataError = document.querySelector(`.${inputError}`)
     formDataError.toggleAttribute("data-error")
@@ -80,16 +91,32 @@ function Validate() {
   }
   function setAttribute(msg, inputError) {
     let formDataError = document.querySelector(`.${inputError}`)
-      formDataError.setAttribute("data-error", msg)
-      formDataError.setAttribute("data-error-visible","true")
+    formDataError.setAttribute("data-error", msg)
+    formDataError.setAttribute("data-error-visible","true")
   }
 
-  checkinput(regexprenom, msgprenom, first.value, "first")
-  checkinput(regexnom, msgnom, last.value, "last")
-  checkinput(regexemail, msgemail, email.value, "email")
-  checkinput(regexquantity, msgquantity, quantity.value, "quantity")
+  function controleform() {
+    if(inputfirst && inputlast && inputemail && inputquantity && inputbirtdate && inputlocation && inputcheckbox1){
+      document.querySelector("form").style.display = "none"
+      let divconfirm = document.createElement("div")
+      let btnclose = document.createElement("button")
+      let textconfirm = document.createElement("span")
+      document.querySelector(".modal-body").appendChild(divconfirm)
+      divconfirm.className = "contentconfirm"
+      document.querySelector(".contentconfirm").appendChild(textconfirm)
+      document.querySelector(".contentconfirm").appendChild(btnclose)
+      textconfirm.innerHTML= "<p>Merci pour<br></br> votre inscription !</p>"
+      btnclose.className = "btn-submit"
+    }
+  }
+
+  // appel des fonctions de controle
+  let inputfirst = checkinput(regexprenom, msgprenom, first.value, "first")
+  let inputlast = checkinput(regexnom, msgnom, last.value, "last")
+  let inputemail = checkinput(regexemail, msgemail, email.value, "email")
+  let inputquantity = checkinput(regexquantity, msgquantity, quantity.value, "quantity")
   checkbirthdate(msgbirthdate, birthdate.value, "birthdate")
-  checklocation(msglocation, location, "radio")
-  checkcheckbox1(msgcheckbox1, checkbox1.checked, "checkbox1" )
-  //////////////////////////////////////////////////////////////////////////////
+  checklocation(msglocation, "radio")
+  checkcheckbox1(msgcheckbox1, checkbox1.checked, "checkbox1")
+  controleform()
 }
